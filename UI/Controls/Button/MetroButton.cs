9b9691740e51df9055d164717ddf63b490c5075e
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-09-22 19:22:50 -0400

    added styles;

---
+++ UI/Controls/Button/MetroButton.cs
@@ -0,0 +1,147 @@
+﻿// ******************************************************************************************
+//     Assembly:                Badger
+//     Author:                  Terry D. Eppler
+//     Created:                 08-01-2024
+// 
+//     Last Modified By:        Terry D. Eppler
+//     Last Modified On:        08-01-2024
+// ******************************************************************************************
+// <copyright file="MetroButton.cs" company="Terry D. Eppler">
+//    Badger is data analysis and reporting tool for EPA Analysts
+//    based on WPF, NET6.0, and written in C-Sharp.
+// 
+//    Copyright ©  2024  Terry D. Eppler
+// 
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the “Software”),
+//    to deal in the Software without restriction,
+//    including without limitation the rights to use,
+//    copy, modify, merge, publish, distribute, sublicense,
+//    and/or sell copies of the Software,
+//    and to permit persons to whom the Software is furnished to do so,
+//    subject to the following conditions:
+// 
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+// 
+//    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+//    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//    DEALINGS IN THE SOFTWARE.
+// 
+//    You can contact me at: terryeppler@gmail.com or eppler.terry@epa.gov
+// </copyright>
+// <summary>
+//   MetroButton.cs
+// </summary>
+// ******************************************************************************************
+
+namespace Ninja
+{
+    using Syncfusion.Windows.Tools.Controls;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Input;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// </summary>
+    /// <seealso cref="T:System.Windows.Controls.Canvas" />
+    [ SuppressMessage( "ReSharper", "UnusedType.Global" ) ]
+    [ SuppressMessage( "ReSharper", "MemberCanBePrivate.Global" ) ]
+    [ SuppressMessage( "ReSharper", "InconsistentNaming" ) ]
+    [ SuppressMessage( "ReSharper", "MemberCanBeInternal" ) ]
+    [ SuppressMessage( "ReSharper", "ClassCanBeSealed.Global" ) ]
+    [ SuppressMessage( "ReSharper", "ClassNeverInstantiated.Global" ) ]
+    public class MetroButton : ButtonAdv
+    {
+        /// <summary>
+        /// The theme
+        /// </summary>
+        private protected readonly DarkMode _theme = new DarkMode( );
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="T:Badger.Button" /> class.
+        /// </summary>
+        public MetroButton( )
+            : base( )
+        {
+            // Control Properties
+            SetResourceReference( MetroButton.StyleProperty, typeof( ButtonAdv ) );
+            Width = 140;
+            Height = 50;
+            IconHeight = 16;
+            IconWidth = 16;
+            SizeMode = SizeMode.Normal;
+            Background = _theme.ControlBackground;
+            Foreground = _theme.LightBlueBrush;
+            BorderBrush = _theme.ControlBackground;
+            Margin = _theme.Margin;
+            Padding = _theme.Padding;
+            BorderThickness = _theme.BorderThickness;
+
+            // Wire Events
+            MouseEnter += OnMouseEnter;
+            MouseLeave += OnMouseLeave;
+        }
+
+        /// <summary> Called when [mouse enter]. </summary>
+        /// <param name="sender"> The sender. </param>
+        /// <param name="e">
+        /// The
+        /// <see cref="EventArgs"/>
+        /// instance containing the event data.
+        /// </param>
+        private protected virtual void OnMouseEnter( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                Background = _theme.SteelBlueBrush;
+                BorderBrush = _theme.LightBlueBrush;
+                Foreground = _theme.WhiteForeground;
+            }
+            catch(Exception ex)
+            {
+                Fail(ex);
+            }
+        }
+
+        /// <summary> Called when [mouse leave]. </summary>
+        /// <param name="sender"> The sender. </param>
+        /// <param name="e">
+        /// The
+        /// <see cref="EventArgs"/>
+        /// instance containing the event data.
+        /// </param>
+        private protected virtual void OnMouseLeave( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                Background = _theme.ControlBackground;
+                BorderBrush = _theme.ControlBackground;
+                Foreground = _theme.LightBlueBrush;
+            }
+            catch(Exception ex)
+            {
+                Fail(ex);
+            }
+        }
+
+        /// <summary>
+        /// Fails the specified ex.
+        /// </summary>
+        /// <param name="ex">The ex.</param>
+        private protected void Fail( Exception ex )
+        {
+            var _error = new ErrorWindow( ex );
+            _error?.SetText( );
+            _error?.ShowDialog( );
+        }
+    }
+}
