7acbccf43a630fb262d3ed277189f882be10f9ff
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-09-24 11:01:19 -0400

    update context mnu

--- Resources/Github/Compilation.md
+++ Resources/Github/Compilation.md
@@ -1,8 +1,8 @@
-# How to compile Badger
+# How to compile Ninja
 
-#### Step 1: Fork a Badger
+#### Step 1: Fork a Ninja
 
-Fork the Badger repository on GitHub.
+Fork the Ninja repository on GitHub.
 
 #### Step 2: Install Visual Studio
 
@@ -18,12 +18,12 @@ Open VS Community and select "Clone a repository"
 
 #### Step 3: Clone this project
 
-Type in your Badger fork URL and press "Clone"
+Type in your Ninja fork URL and press "Clone"
 
 ![image](https://user-images.githubusercontent.com/104514709/183603880-92217d8a-bc99-4adf-b6d7-030096576090.png)
 
 #### Step 4: Develop!
 
-You now have a Badger! Press "Start" after you choose the system architecture
+You now have a Ninja! Press "Start" after you choose the system architecture
 
 ![image](https://user-images.githubusercontent.com/104514709/183604128-771daefc-6254-41bc-8660-2d28200a0b1e.png)
--- UI/Windows/Iperf/IperfWindow.xaml.cs
+++ UI/Windows/Iperf/IperfWindow.xaml.cs
@@ -1,4 +1,44 @@
-
+// ******************************************************************************************
+//     Assembly:                Ninja
+//     Author:                  Terry D. Eppler
+//     Created:                 09-24-2024
+// 
+//     Last Modified By:        Terry D. Eppler
+//     Last Modified On:        09-24-2024
+// ******************************************************************************************
+// <copyright file="IperfWindow.xaml.cs" company="Terry D. Eppler">
+// 
+//    Ninja is a network toolkit, support iperf, tcp, udp, websocket, mqtt,
+//    sniffer, pcap, port scan, listen, ip scan .etc.
+// 
+//    Copyright ©  2019-2024 Terry D. Eppler
+// 
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the “Software”),
+//    to deal in the Software without restriction,
+//    including without limitation the rights to use,
+//    copy, modify, merge, publish, distribute, sublicense,
+//    and/or sell copies of the Software,
+//    and to permit persons to whom the Software is furnished to do so,
+//    subject to the following conditions:
+// 
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+// 
+//    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+//    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//    DEALINGS IN THE SOFTWARE.
+// 
+//    You can contact me at:  terryeppler@gmail.com or eppler.terry@epa.gov
+// </copyright>
+// <summary>
+//   IperfWindow.xaml.cs
+// </summary>
+// ******************************************************************************************
 
 namespace Ninja.Views
 {
@@ -27,7 +67,7 @@ namespace Ninja.Views
         /// <summary>
         /// The path
         /// </summary>
-        private protected object _entry = new object();
+        private protected object _entry = new object( );
 
         /// <summary>
         /// The time
@@ -44,11 +84,6 @@ namespace Ninja.Views
         /// </summary>
         private protected TimerCallback _timerCallback;
 
-        public IperfWindow()
-        {
-            InitializeComponent();
-        }
-        
         /// <summary>
         /// Called when [calculator menu option click].
         /// </summary>
@@ -238,11 +273,11 @@ namespace Ninja.Views
         /// and unmanaged resources;
         /// <c>false</c> to release only unmanaged resources.
         /// </param>
-        protected virtual void Dispose(bool disposing)
+        protected virtual void Dispose( bool disposing )
         {
-            if(disposing)
+            if( disposing )
             {
-                _timer?.Dispose();
+                _timer?.Dispose( );
             }
         }
 
@@ -254,19 +289,24 @@ namespace Ninja.Views
         /// </summary>
         public void Dispose( )
         {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            Dispose( true );
+            GC.SuppressFinalize( this );
         }
 
         /// <summary>
         /// Fails the specified ex.
         /// </summary>
         /// <param name="ex">The ex.</param>
-        private protected void Fail(Exception ex)
+        private protected void Fail( Exception ex )
         {
-            var _error = new ErrorWindow(ex);
-            _error?.SetText();
-            _error?.ShowDialog();
+            var _error = new ErrorWindow( ex );
+            _error?.SetText( );
+            _error?.ShowDialog( );
+        }
+
+        public IperfWindow( )
+        {
+            InitializeComponent( );
         }
     }
 }
