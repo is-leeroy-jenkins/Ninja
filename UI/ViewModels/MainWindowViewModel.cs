84c6080dad12d530a4f301dd1706ffee0d5fa9e8
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-09-26 08:21:01 -0400

    up

--- UI/ViewModels/MainWindowViewModel.cs
+++ UI/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,10 @@
 ﻿// ******************************************************************************************
 //     Assembly:                Ninja
 //     Author:                  Terry D. Eppler
-//     Created:                 09-22-2024
+//     Created:                 09-26-2024
 // 
 //     Last Modified By:        Terry D. Eppler
-//     Last Modified On:        09-22-2024
+//     Last Modified On:        09-26-2024
 // ******************************************************************************************
 // <copyright file="MainWindowViewModel.cs" company="Terry D. Eppler">
 // 
@@ -55,19 +55,56 @@ namespace Ninja.ViewModels
     /// <inheritdoc />
     /// <summary>
     /// </summary>
-    /// <seealso cref="T:Ninja.ViewModels.MainWindowBase" />
+    /// <seealso cref="MainWindowBase" />
     [ SuppressMessage( "ReSharper", "ClassCanBeSealed.Global" ) ]
     public class MainWindowViewModel : MainWindowBase
     {
-        // CollectionViewSource enables XAML code to set the commonly used CollectionView properties,
-        // passing these settings to the underlying view.
+        /// <summary>
+        /// The iperf view model
+        /// </summary>
+        private protected IperfViewModel _iperfViewModel;
+
+        /// <summary>
+        /// The network scan view model
+        /// </summary>
+        private protected NetworkScanViewModel _networkScanViewModel;
+
+        /// <summary>
+        /// The port scan view model
+        /// </summary>
+        private protected PortScanViewModel _portScanViewModel;
+
+        /// <summary>
+        /// The route view model
+        /// </summary>
+        private protected RouteViewModel _routeViewModel;
+
+        /// <summary>
+        /// The port view model
+        /// </summary>
+        private protected PortViewModel _portViewModel;
+
+        /// <summary>
+        /// The server view model
+        /// </summary>
+        private protected ServerViewModel _serverViewModel;
+
+        /// <summary>
+        /// The sniffer view model
+        /// </summary>
+        private protected SnifferViewModel _snifferViewModel;
+
         /// <summary>
         /// The menu items collection
+        /// CollectionViewSource enables XAML code to set the
+        /// commonly used CollectionView properties,
+        /// passing these settings to the underlying view.
         /// </summary>
         private CollectionViewSource _menuItemsCollection;
 
-        // ICollectionView enables collections to have the functionalities of current record management,
-        // custom sorting, filtering, and grouping.
+        /// ICollectionView enables collections to have the
+        /// functionalities of current record management,
+        /// custom sorting, filtering, and grouping.
         /// <summary>
         /// Gets the source collection.
         /// </summary>
@@ -108,27 +145,58 @@ namespace Ninja.ViewModels
         }
 
         /// <summary>
-        /// Handles the Filter event of the MenuItems control.
+        /// Initializes a new instance of the
+        /// <see cref="MainWindowViewModel"/> class.
         /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="FilterEventArgs"/> instance containing the event data.</param>
-        private void MenuItems_Filter( object sender, FilterEventArgs e )
+        public MainWindowViewModel( )
         {
-            if( string.IsNullOrEmpty( FilterText ) )
+            var _menuItems = new ObservableCollection<MenuItems>
             {
-                e.Accepted = true;
-                return;
-            }
+                new MenuItems
+                {
+                    MenuName = "Iperf",
+                    MenuImage = @"/Resources/Assets/ApplicationImages/Speed.png"
+                },
+                new MenuItems
+                {
+                    MenuName = "NetworkScan",
+                    MenuImage = @"/Resources/Assets/ApplicationImages/IP.png"
+                },
+                new MenuItems
+                {
+                    MenuName = "PortScan",
+                    MenuImage = @"/Resources/Assets/ApplicationImages/port.png"
+                },
+                new MenuItems
+                {
+                    MenuName = "RouteTable",
+                    MenuImage = @"/Resources/Assets/ApplicationImages/route.png"
+                },
+                new MenuItems
+                {
+                    MenuName = "PortListen",
+                    MenuImage = @"/Resources/Assets/ApplicationImages/portlisten.png"
+                },
+                new MenuItems
+                {
+                    MenuName = "Server",
+                    MenuImage = @"/Resources/Assets/ApplicationImages/servers.png"
+                },
+                new MenuItems
+                {
+                    MenuName = "Sniffer",
+                    MenuImage = @"/Resources/Assets/ApplicationImages/sniffer.png"
+                }
+            };
 
-            var _item = e.Item as MenuItems;
-            if( _item.MenuName.ToUpper( ).Contains( FilterText.ToUpper( ) ) )
+            _menuItemsCollection = new CollectionViewSource
             {
-                e.Accepted = true;
-            }
-            else
-            {
-                e.Accepted = false;
-            }
+                Source = _menuItems
+            };
+
+            _menuItemsCollection.Filter += OnMenuItemsFilter;
+            _iperfViewModel = new IperfViewModel( );
+            _selectedViewModel = _iperfViewModel;
         }
 
         /// <summary>
@@ -163,14 +231,19 @@ namespace Ninja.ViewModels
             switch( parameter )
             {
                 case "Calc":
+                {
                     Process.Start( "calc.exe" );
                     break;
+                }
                 case "About":
+                {
                     var _aboutWindow = new AboutWindow( );
                     _aboutWindow.Show( );
                     Dispatcher.Run( );
                     break;
+                }
                 case "Iperf":
+                {
                     var _iperfInfo = new ProcessStartInfo( "https://iperf.fr/" )
                     {
                         UseShellExecute = true
@@ -178,15 +251,19 @@ namespace Ninja.ViewModels
 
                     Process.Start( _iperfInfo );
                     break;
+                }
                 case "Pcap":
+                {
                     var _pcapInfo =
-                        new ProcessStartInfo( "https://www.tcpdump.org/manpages/pcap-filter.7.html" )
+                        new ProcessStartInfo(
+                            "https://www.tcpdump.org/manpages/pcap-filter.7.html" )
                         {
                             UseShellExecute = true
                         };
 
                     Process.Start( _pcapInfo );
                     break;
+                }
             }
         }
 
@@ -258,37 +335,135 @@ namespace Ninja.ViewModels
         /// <summary>
         /// The iperf view model
         /// </summary>
-        public IperfViewModel IperfViewModel;
+        public IperfViewModel IperfViewModel
+        {
+            get
+            {
+                return _iperfViewModel;
+            }
+            private protected set
+            {
+                if( _iperfViewModel != value )
+                {
+                    _iperfViewModel = value;
+                    OnPropertyChanged( nameof( IperfViewModel ) );
+                }
+            }
+        }
 
         /// <summary>
         /// The network scan view model
         /// </summary>
-        public NetworkScanViewModel NetworkScanViewModel;
+        public NetworkScanViewModel NetworkScanViewModel
+        {
+            get
+            {
+                return _networkScanViewModel;
+            }
+            private protected set
+            {
+                if( _networkScanViewModel != value )
+                {
+                    _networkScanViewModel = value;
+                    OnPropertyChanged( nameof( NetworkScanViewModel ) );
+                }
+            }
+        }
 
         /// <summary>
         /// The port scan view model
         /// </summary>
-        public PortScanViewModel PortScanViewModel;
+        public PortScanViewModel PortScanViewModel
+        {
+            get
+            {
+                return _portScanViewModel;
+            }
+            private protected set
+            {
+                if( _portScanViewModel != value )
+                {
+                    _portScanViewModel = value;
+                    OnPropertyChanged( nameof( PortScanViewModel ) );
+                }
+            }
+        }
 
         /// <summary>
         /// The route view model
         /// </summary>
-        public RouteViewModel RouteViewModel;
+        public RouteViewModel RouteViewModel
+        {
+            get
+            {
+                return _routeViewModel;
+            }
+            private protected set
+            {
+                if( _routeViewModel != value )
+                {
+                    _routeViewModel = value;
+                    OnPropertyChanged( nameof( RouteViewModel ) );
+                }
+            }
+        }
 
         /// <summary>
         /// The port view model
         /// </summary>
-        public PortViewModel PortViewModel;
+        public PortViewModel PortViewModel
+        {
+            get
+            {
+                return _portViewModel;
+            }
+            private protected set
+            {
+                if( _portViewModel != value )
+                {
+                    _portViewModel = value;
+                    OnPropertyChanged( nameof( PortScanViewModel ) );
+                }
+            }
+        }
 
         /// <summary>
         /// The server view model
         /// </summary>
-        public ServerViewModel ServerViewModel;
+        public ServerViewModel ServerViewModel
+        {
+            get
+            {
+                return _serverViewModel;
+            }
+            private protected set
+            {
+                if( _serverViewModel != value )
+                {
+                    _serverViewModel = value;
+                    OnPropertyChanged( nameof( ServerViewModel ) );
+                }
+            }
+        }
 
         /// <summary>
         /// The sniffer view model
         /// </summary>
-        public SnifferViewModel SnifferViewModel;
+        public SnifferViewModel SnifferViewModel
+        {
+            get
+            {
+                return _snifferViewModel;
+            }
+            private protected set
+            {
+                if( _snifferViewModel != value )
+                {
+                    _snifferViewModel = value;
+                    OnPropertyChanged( nameof( SnifferViewModel ) );
+                }
+            }
+        }
 
         /// <summary>
         /// Switches the views.
@@ -358,57 +533,27 @@ namespace Ninja.ViewModels
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// Handles the Filter event of the MenuItems control.
         /// </summary>
-        public MainWindowViewModel( )
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="FilterEventArgs"/> instance containing the event data.</param>
+        private void OnMenuItemsFilter( object sender, FilterEventArgs e )
         {
-            var _menuItems = new ObservableCollection<MenuItems>
+            if( string.IsNullOrEmpty( FilterText ) )
             {
-                new MenuItems
-                {
-                    MenuName = "Iperf",
-                    MenuImage = @"/Resources/Assets/ApplicationImages/Speed.png"
-                },
-                new MenuItems
-                {
-                    MenuName = "NetworkScan",
-                    MenuImage = @"/Resources/Assets/ApplicationImages/IP.png"
-                },
-                new MenuItems
-                {
-                    MenuName = "PortScan",
-                    MenuImage = @"/Resources/Assets/ApplicationImages/port.png"
-                },
-                new MenuItems
-                {
-                    MenuName = "RouteTable",
-                    MenuImage = @"/Resources/Assets/ApplicationImages/route.png"
-                },
-                new MenuItems
-                {
-                    MenuName = "PortListen",
-                    MenuImage = @"/Resources/Assets/ApplicationImages/portlisten.png"
-                },
-                new MenuItems
-                {
-                    MenuName = "Server",
-                    MenuImage = @"/Resources/Assets/ApplicationImages/servers.png"
-                },
-                new MenuItems
-                {
-                    MenuName = "Sniffer",
-                    MenuImage = @"/Resources/Assets/ApplicationImages/sniffer.png"
-                }
-            };
+                e.Accepted = true;
+                return;
+            }
 
-            _menuItemsCollection = new CollectionViewSource
+            var _item = e.Item as MenuItems;
+            if( _item.MenuName.ToUpper( ).Contains( FilterText.ToUpper( ) ) )
             {
-                Source = _menuItems
-            };
-
-            _menuItemsCollection.Filter += MenuItems_Filter;
-            IperfViewModel = new IperfViewModel( );
-            SelectedViewModel = IperfViewModel;
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
         }
     }
 }
