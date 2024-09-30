f1917fa1c9858312bf373cfb44479625d5bbde01
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-09-30 10:40:02 -0400

    up

--- UI/Controls/TextBox/TextBoxEx.cs
+++ UI/Controls/TextBox/TextBoxEx.cs
@@ -1,15 +1,15 @@
 ﻿// ******************************************************************************************
 //     Assembly:                Ninja
 //     Author:                  Terry D. Eppler
-//     Created:                 09-25-2024
+//     Created:                 09-30-2024
 // 
 //     Last Modified By:        Terry D. Eppler
-//     Last Modified On:        09-25-2024
+//     Last Modified On:        09-30-2024
 // ******************************************************************************************
 // <copyright file="TextBoxEx.cs" company="Terry D. Eppler">
 // 
-//    Ninja is a network toolkit, support iperf, tcp, udp, websocket, mqtt,
-//    sniffer, pcap, port scan, listen, ip scan .etc.
+//     Ninja is a network toolkit that supports Iperf, TCP, UDP, Websocket, MQTT,
+//     Sniffer, Pcap, Port Scan, Listen, IP Scan .etc.
 // 
 //    Copyright ©  2019-2024 Terry D. Eppler
 // 
@@ -40,203 +40,140 @@
 // </summary>
 // ******************************************************************************************
 
-
-
 namespace Ninja
 {
     using System;
-    using System.Collections.Generic;
-    using System.Globalization;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Controls.Primitives;
-    using System.Windows.Data;
-    using System.Windows.Documents;
     using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
 
-
+    /// <inheritdoc />
     /// <summary>
-    /// 
     /// </summary>
-    /// <seealso cref="System.Windows.Data.IValueConverter" />
-    public class TextBoxExConverter : IValueConverter
-    {
-        public object Convert( object value, Type targetType, object parameter,
-            CultureInfo culture )
-        {
-            return ( bool )value
-                ? Visibility.Visible
-                : Visibility.Collapsed;
-        }
-
-        public object ConvertBack( object value, Type targetType, object parameter,
-            CultureInfo culture )
-        {
-            return DependencyProperty.UnsetValue;
-        }
-    }
-
+    /// <seealso cref="System.Windows.Controls.TextBox" />
     public class TextBoxEx : TextBox
     {
-        static TextBoxEx( )
-        {
-            DefaultStyleKeyProperty.OverrideMetadata( typeof( TextBoxEx ),
-                new FrameworkPropertyMetadata( typeof( TextBoxEx ) ) );
-        }
-
-        public double Step
-        {
-            get { return ( double )GetValue( StepProperty ); }
-            set { SetValue( StepProperty, value ); }
-        }
-
+        /// <summary>
+        /// The step property
+        /// </summary>
         public static readonly DependencyProperty StepProperty =
             DependencyProperty.Register( "Step", typeof( double ), typeof( TextBoxEx ),
                 new PropertyMetadata( 1.0 ) );
 
-        public int Minimum
-        {
-            get { return ( int )GetValue( MinimumProperty ); }
-            set { SetValue( MinimumProperty, value ); }
-        }
-
+        /// <summary>
+        /// The minimum property
+        /// </summary>
         public static readonly DependencyProperty MinimumProperty =
             DependencyProperty.Register( "Minimum", typeof( int ), typeof( TextBoxEx ),
                 new PropertyMetadata( 0 ) );
 
-        public int Maximum
-        {
-            get { return ( int )GetValue( MaximumProperty ); }
-            set { SetValue( MaximumProperty, value ); }
-        }
-
+        /// <summary>
+        /// The maximum property
+        /// </summary>
         public static readonly DependencyProperty MaximumProperty =
             DependencyProperty.Register( "Maximum", typeof( int ), typeof( TextBoxEx ),
                 new PropertyMetadata( 0 ) );
 
-        #region CornerRadius
-        public CornerRadius GetCornerRadius( DependencyObject obj )
-        {
-            return ( CornerRadius )obj.GetValue( CornerRadiusProperty );
-        }
-
-        public static void SetCornerRadius( DependencyObject obj, CornerRadius value )
-        {
-            obj.SetValue( CornerRadiusProperty, value );
-        }
-
+        /// <summary>
+        /// The corner radius property
+        /// </summary>
         public static readonly DependencyProperty CornerRadiusProperty =
             DependencyProperty.RegisterAttached( "CornerRadius", typeof( CornerRadius ),
                 typeof( TextBoxEx ) );
-        #endregion
 
-        #region IsClearButtonVisible
-        public static bool GetIsClearBtnVisible( DependencyObject obj )
-        {
-            return ( bool )obj.GetValue( IsClearBtnVisibleProperty );
-        }
-
-        public static void SetIsClearBtnVisible( DependencyObject obj, bool value )
-        {
-            obj.SetValue( IsClearBtnVisibleProperty, value );
-        }
-
-        public static readonly DependencyProperty IsClearBtnVisibleProperty =
-            DependencyProperty.RegisterAttached( "IsClearBtnVisible", typeof( bool ),
+        /// <summary>
+        /// The is clear button visible property
+        /// </summary>
+        public static readonly DependencyProperty IsClearButtonVisibleProperty =
+            DependencyProperty.RegisterAttached( "IsClearButtonVisible", typeof( bool ),
                 typeof( TextBoxEx ), new PropertyMetadata( TextBoxEx.OnTextBoxHookChanged ) );
 
-        private static void OnTextBoxHookChanged( DependencyObject d,
-            DependencyPropertyChangedEventArgs e )
-        {
-            var _textbox = d as TextBox;
-            _textbox.RemoveHandler( ButtonBase.ClickEvent,
-                new RoutedEventHandler( TextBoxEx.ClearBtnClicked ) );
-
-            _textbox.AddHandler( ButtonBase.ClickEvent,
-                new RoutedEventHandler( TextBoxEx.ClearBtnClicked ) );
-        }
-
-        private static void ClearBtnClicked( object sender, RoutedEventArgs e )
-        {
-            var _button = e.OriginalSource as Button;
-            if( _button == null
-                || _button.Name != "PART_BtnClear" )
-            {
-                return;
-            }
-
-            var _textbox = sender as TextBox;
-            if( _textbox == null )
-            {
-                return;
-            }
-
-            _textbox.Text = "";
-        }
-        #endregion
-
-        public static bool GetIsAddBtnVisible( DependencyObject obj )
-        {
-            return ( bool )obj.GetValue( IsAddBtnVisibleProperty );
-        }
-
-        public static void SetIsAddBtnVisible( DependencyObject obj, bool value )
-        {
-            obj.SetValue( IsAddBtnVisibleProperty, value );
-        }
-
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IsAddBtnVisibleProperty =
-            DependencyProperty.RegisterAttached( "IsAddBtnVisible", typeof( bool ),
+        /// <summary>
+        /// The is add button visible property
+        /// </summary>
+        public static readonly DependencyProperty IsAddButtonVisibleProperty =
+            DependencyProperty.RegisterAttached( "IsAddButtonVisible", typeof( bool ),
                 typeof( TextBoxEx ), new PropertyMetadata( TextBoxEx.OnTextChanged ) );
 
-        public static bool GetIsRemoveBtnVisible( DependencyObject obj )
-        {
-            return ( bool )obj.GetValue( IsRemoveBtnVisibleProperty );
-        }
-
-        public static void SetIsRemoveBtnVisible( DependencyObject obj, bool value )
-        {
-            obj.SetValue( IsRemoveBtnVisibleProperty, value );
-        }
-
-        public static readonly DependencyProperty IsRemoveBtnVisibleProperty =
-            DependencyProperty.RegisterAttached( "IsRemoveBtnVisible", typeof( bool ),
+        /// <summary>
+        /// The is remove button visible property
+        /// </summary>
+        public static readonly DependencyProperty IsRemoveButtonVisibleProperty =
+            DependencyProperty.RegisterAttached( "IsRemoveButtonVisible", typeof( bool ),
                 typeof( TextBoxEx ), new PropertyMetadata( TextBoxEx.OnTextChanged ) );
 
+        /// <summary>
+        /// Gets the is add button visible.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static bool GetIsAddButtonVisible( DependencyObject obj )
+        {
+            return ( bool )obj.GetValue( IsAddButtonVisibleProperty );
+        }
+
+        /// <summary>
+        /// Sets the is add button visible.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        public static void SetIsAddButtonVisible( DependencyObject obj, bool value )
+        {
+            obj.SetValue( IsAddButtonVisibleProperty, value );
+        }
+
+        /// <summary>
+        /// Gets the is remove button visible.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static bool GetIsRemoveButtonVisible( DependencyObject obj )
+        {
+            return ( bool )obj.GetValue( IsRemoveButtonVisibleProperty );
+        }
+
+        /// <summary>
+        /// Sets the is remove button visible.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        public static void SetIsRemoveButtonVisible( DependencyObject obj, bool value )
+        {
+            obj.SetValue( IsRemoveButtonVisibleProperty, value );
+        }
+
+        /// <summary>
+        /// Called when [text changed].
+        /// </summary>
+        /// <param name="d">The d.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private static void OnTextChanged( DependencyObject d,
             DependencyPropertyChangedEventArgs e )
         {
             var _textbox = d as TextBox;
             if( ( bool )e.NewValue )
             {
-                _textbox.MouseWheel += TextBoxEx.Textbox_MouseWheel;
+                _textbox.MouseWheel += TextBoxEx.OnTextboxMouseWheel;
             }
             else
             {
-                _textbox.MouseWheel -= TextBoxEx.Textbox_MouseWheel;
+                _textbox.MouseWheel -= TextBoxEx.OnTextboxMouseWheel;
             }
 
             _textbox.RemoveHandler( ButtonBase.ClickEvent,
-                new RoutedEventHandler( TextBoxEx.ButtonClicked ) );
+                new RoutedEventHandler( TextBoxEx.OnButtonClicked ) );
 
             _textbox.AddHandler( ButtonBase.ClickEvent,
-                new RoutedEventHandler( TextBoxEx.ButtonClicked ) );
+                new RoutedEventHandler( TextBoxEx.OnButtonClicked ) );
         }
 
         /// <summary>
-        /// 鼠标滚轮
+        /// Called when [textbox mouse wheel].
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private static void Textbox_MouseWheel( object sender, MouseWheelEventArgs e )
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MouseWheelEventArgs"/> instance containing the event data.</param>
+        private static void OnTextboxMouseWheel( object sender, MouseWheelEventArgs e )
         {
             var _textboxex = e.Source as TextBoxEx;
             double _step = 1;
@@ -266,11 +203,16 @@ namespace Ninja
                     _textbox.Text = _min.ToString( );
                 }
 
-                _textbox.Select( _textbox.Text.Length, 0 );//光标设置到文本尾部
+                _textbox.Select( _textbox.Text.Length, 0 );
             }
         }
 
-        private static void ButtonClicked( object sender, RoutedEventArgs e )
+        /// <summary>
+        /// Called when [button clicked].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private static void OnButtonClicked( object sender, RoutedEventArgs e )
         {
             var _button = e.OriginalSource as RepeatButton;
             if( _button == null )
@@ -294,8 +236,6 @@ namespace Ninja
             }
 
             double _temp;
-
-            //int Step = step.Step;
             var _result = double.TryParse( _textbox.Text, out _temp );
             if( _result )
             {
@@ -317,5 +257,134 @@ namespace Ninja
                 _textbox.Select( _textbox.Text.Length, 0 );
             }
         }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes the <see cref="Ninja.TextBoxEx" /> class.
+        /// </summary>
+        static TextBoxEx( )
+        {
+            DefaultStyleKeyProperty.OverrideMetadata( typeof( TextBoxEx ),
+                new FrameworkPropertyMetadata( typeof( TextBoxEx ) ) );
+        }
+
+        /// <summary>
+        /// Gets or sets the step.
+        /// </summary>
+        /// <value>
+        /// The step.
+        /// </value>
+        public double Step
+        {
+            get { return ( double )GetValue( StepProperty ); }
+            set { SetValue( StepProperty, value ); }
+        }
+
+        /// <summary>
+        /// Gets or sets the minimum.
+        /// </summary>
+        /// <value>
+        /// The minimum.
+        /// </value>
+        public int Minimum
+        {
+            get { return ( int )GetValue( MinimumProperty ); }
+            set { SetValue( MinimumProperty, value ); }
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum.
+        /// </summary>
+        /// <value>
+        /// The maximum.
+        /// </value>
+        public int Maximum
+        {
+            get { return ( int )GetValue( MaximumProperty ); }
+            set { SetValue( MaximumProperty, value ); }
+        }
+
+        #region CornerRadius
+        /// <summary>
+        /// Gets the corner radius.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public CornerRadius GetCornerRadius( DependencyObject obj )
+        {
+            return ( CornerRadius )obj.GetValue( CornerRadiusProperty );
+        }
+
+        /// <summary>
+        /// Sets the corner radius.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <param name="value">The value.</param>
+        public static void SetCornerRadius( DependencyObject obj, CornerRadius value )
+        {
+            obj.SetValue( CornerRadiusProperty, value );
+        }
+        #endregion
+
+        #region IsClearButtonVisible
+        /// <summary>
+        /// Gets the is clear button visible.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static bool GetIsClearButtonVisible( DependencyObject obj )
+        {
+            return ( bool )obj.GetValue( IsClearButtonVisibleProperty );
+        }
+
+        /// <summary>
+        /// Sets the is clear button visible.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        public static void SetIsClearButtonVisible( DependencyObject obj, bool value )
+        {
+            obj.SetValue( IsClearButtonVisibleProperty, value );
+        }
+
+        /// <summary>
+        /// Called when [text box hook changed].
+        /// </summary>
+        /// <param name="d">The d.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
+        private static void OnTextBoxHookChanged( DependencyObject d,
+            DependencyPropertyChangedEventArgs e )
+        {
+            var _textbox = d as TextBox;
+            _textbox.RemoveHandler( ButtonBase.ClickEvent,
+                new RoutedEventHandler( TextBoxEx.OnClearButtonClicked ) );
+
+            _textbox.AddHandler( ButtonBase.ClickEvent,
+                new RoutedEventHandler( TextBoxEx.OnClearButtonClicked ) );
+        }
+
+        /// <summary>
+        /// Called when [clear button clicked].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private static void OnClearButtonClicked( object sender, RoutedEventArgs e )
+        {
+            var _button = e.OriginalSource as Button;
+            if( _button == null
+                || _button.Name != "PART_BtnClear" )
+            {
+                return;
+            }
+
+            var _textbox = sender as TextBox;
+            if( _textbox == null )
+            {
+                return;
+            }
+
+            _textbox.Text = "";
+        }
+        #endregion
     }
 }
